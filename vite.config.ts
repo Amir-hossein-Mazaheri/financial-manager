8693f43eb15653d432cc7d9f6d504e239e556328
Amirhossein Mazaheri <uplayggboy@gmail.com>
2023-09-08 21:02:03 +0330

    added electron and electron forge for making desktop app

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import UnoCSS from "unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.IS_DEV !== "true" ? "./" : "/",
+  build: {
+    outDir: "build",
+  },
   plugins: [react(), UnoCSS()],
 });
