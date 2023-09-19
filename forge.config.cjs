10fad3235e95ceb7618ad2cba379a2e01522ea87
Amirhossein Mazaheri <uplayggboy@gmail.com>
2023-09-19 22:59:30 +0330

    added custom icon for packaged output

--- forge.config.cjs
+++ forge.config.cjs
@@ -1,13 +1,19 @@
 module.exports = {
-  packagerConfig: {
-    asar: true,
-  },
   rebuildConfig: {},
+  packagerConfig: {
+    name: "Financial Manager",
+    icon: "./src/assets/icon",
+    platform: "all",
+    ignore: "./prisma/dev.db",
+  },
   makers: [
     {
       name: "@electron-forge/maker-squirrel",
       config: {
         noMsi: false,
+        iconUrl:
+          "https://s123.convertio.me/p/LpGmOat0UYRqvDhB8qE0bg/e5f822b67e7bce1291346cb9e3b76510/icon.ico",
+        setupIcon: "./src/assets/icon.ico",
       },
     },
     {
@@ -20,12 +26,6 @@ module.exports = {
     },
     {
       name: "@electron-forge/maker-rpm",
-      config: {},
-    },
-  ],
-  plugins: [
-    {
-      name: "@electron-forge/plugin-auto-unpack-natives",
       config: {},
     },
   ],
